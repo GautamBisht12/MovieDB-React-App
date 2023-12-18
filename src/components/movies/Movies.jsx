bc708b8f29ae92a111dad29eb1a66c189acc9df4
Gautam Bisht <141318426+GautamBisht12@users.noreply.github.com>
2023-12-18 16:07:52 +0530

    Update Movies.jsx

--- src/components/movies/Movies.jsx
+++ src/components/movies/Movies.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import SingleContent from "../../components/singleContent/SingleContent";
 import { Box } from "@mui/material";
 import CustomPagination from "../pagination/CustomPagination";
+import SingleContent from "../../components/singleContent/SingleContent";
 import Genre from "../genre/Genre";
 import useGenres from "../../hooks/useGenres";
 
@@ -14,8 +14,8 @@ const Movies = () => {
   const [noOfPages, setNoOfPages] = useState();
   const genreforURL = useGenres(selectedGenres);
 
-  const API_KEY = "68c3ad2897ef4acd7987e0e9218396c0";
   const fetchMovie = async () => {
+  const API_KEY = "68c3ad2897ef4acd7987e0e9218396c0";
     try {
       const { data } = await axios.get(
         `https://api.themoviedb.org/3/discover/movie/?api_key=${API_KEY}&page=${page}&with_genres=${genreforURL}`
